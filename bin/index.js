9d5ecbc39bcf35ab0e3a0669768b15792da12ece
felipemendes <f.ribeiromendes@gmail.com>
2018-09-04 22:01:28 -0300

    Add event filter by search argument from API

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,24 @@
 #! /usr/bin/env node
-var https = require('https')
-var arguments = process.argv.splice(2, process.argv.length -1).join(' ')
-console.log(arguments);
+
+var http = require('http')
+var querystring = require('querystring')
+
+var arguments = process.argv.splice(2, process.argv.length - 1).join(' ')
+var search = querystring.stringify({
+    search: arguments
+})
+
+http
+    .get('http://localhost:3000/events?' + search, function (res) {
+        var data = ''
+
+        res.on('data', function (newData) {
+            data += newData
+        });
+
+        res.on('end', function () {
+            var event = JSON.parse(data)
+            //var event = JSON.parse(data).events[0].title
+            console.log(event)
+        })
+    })
